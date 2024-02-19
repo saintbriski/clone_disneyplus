5c4b3f5dbe4185cd1ada6f9668274d446dc87b6a
Willian Briski <wyll_gustavo11@hotmail.com>
2024-02-19 12:19:22 -0300

    header finalizado

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
-    
 
+    const heroSection = document.querySelector('.hero');
+    const heroHeigh = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const currentPosition = window.scrollY;
+
+        if (currentPosition < heroHeigh) {
+            hiddeElementsFromHeader();
+        } else {
+            showHeaderElements();
+        }
+    })
+    
+    // Seção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const targetTab = botao.target.dataset.tabButton;
@@ -14,10 +27,21 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
+    // Seção FAQ, accordion
     for (let i= 0; i < questions.length; i++) {
         questions[i].addEventListener('click', openOrCloseAnswer);
     }
 })
+
+function hiddeElementsFromHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function showHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 function openOrCloseAnswer(elemento) {
     const classe = 'faq__questions__item--is-open';
